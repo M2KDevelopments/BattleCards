a90245ac85f0a75b2124e06681726337afac6bfd
M2K Developments <m2ksoftiez@gmail.com>
2024-06-13 22:45:24 +0200

    pwa web vite project

--- web/public/content/oauth.js
+++
@@ -1,7 +0,0 @@
-/*global chrome*/
-/** This looks for the OAUTH Screen for to get the access token from the url */
-console.log('CRM Side Kick');
-if (window.location.href.match(/crmsidekick.*oauth/gmi)) {
-    const access_token = document.querySelector(".access_token").textContent;
-    chrome.runtime.sendMessage({ cid: "oauth", access_token });
-}
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,38 +1,35 @@
-import React, { useState, useEffect } from "react"
-import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-// pages
-import PageHome from "./pages/PageHome";
-import PageLogin from "./pages/PageLogin";
-
-// others
-// import * as API from "./utils/api";
-export const ContextFullScreen = React.createContext(false);
 function App() {
- 
-  const [fullscreen, setFullscreen] = useState(false);
-
-  // check for fullscreen
-  useEffect(() => {
-    const fullscreen = window.location.href.includes("fullscreen");
-    setFullscreen(fullscreen);
-    if (fullscreen) document.body.classList.add("fullscreen");
-  }, [])
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="main">
-      <ContextFullScreen.Provider value={fullscreen}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<PageLogin />} />
-            <Route path="/app" element={<PageHome />} />
-          </Routes>
-
-        </Router>
-      </ContextFullScreen.Provider>
-    </div>
-
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
