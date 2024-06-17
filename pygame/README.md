79c0e0487a31b53e78c90aac13afbc90e690df4f
M2K Developments <m2ksoftiez@gmail.com>
2024-06-18 01:13:10 +0200

    update python documentation

---
+++ pygame/README.md
@@ -0,0 +1,15 @@
+# BattleCards
+A Malawi card based game to frustrate the loser of the game.
+
+## Installation and Dependencies
+Make sure you are running python version 3 or above
+
+### Check python version
+```
+python --version
+```
+
+### Install pygame framework
+```
+pip install pygame
+```
--- pygame/main.py
+++ pygame/main.py
@@ -1,5 +1,24 @@
+import pygame
 
 def main():
-    print("Hello World")
+    
+    # create window
+    WIDTH = 800
+    HEIGHT = 600
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    print("Running Battle Cards")
+    
+    # main game loop
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+          
+    # close window            
+    pygame.quit()
 
-main()
+
+# main function entry point
+if __name__ == "__main__":
+    main()
