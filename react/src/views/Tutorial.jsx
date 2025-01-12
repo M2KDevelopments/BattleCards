3161017e14e333e5a8ed9fabad7744de319ba861
M2K Developments <m2ksoftiez@gmail.com>
2025-01-12 18:06:03 +0200

    update

--- react/src/views/Tutorial.jsx
+++ react/src/views/Tutorial.jsx
@@ -1,7 +1,14 @@
-function Tutorial() {
+import DarkOverlay from "../components/DarkOverlay";
+import { ContextData, PAGE_MENU, } from "../App";
+import { useContext } from "react";
 
+
+function Tutorial() {
+    const { setPage } = useContext(ContextData);
     return (
-        <div>
+        <div style={{ backgroundImage: `url('/battlecards.jpg')`, backgroundSize: '100%', overflow: 'hidden' }} className="h-screen flex flex-col gap-4 justify-center items-center align-middle m-auto">
+            <DarkOverlay />
+            <button className='z-10 text-3xl rounded-md px-8 py-2 border-2 border-purple-300 bg-pink-700 hover:bg-pink-900 duration-200 text-white' onClick={() => setPage(PAGE_MENU)}>Back To Main Menu</button>
 
         </div>
     )
